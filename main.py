3b937f9926b135119cfd0473301988356a4c90cc
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-12-12 20:45:54 -0500

    knight movement code

--- main.py
+++ main.py
@@ -87,8 +87,12 @@ def getLegalMoves(board, position, piece):
                 else:
                     break
 
-    elif piece.lower() == 'n':
-        knight_moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+    elif piece.lower() == "n":  # Knight moves
+        knightMoves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+        for dr, dc in knightMoves:
+            newRow, newCol = row + dr, col + dc
+            if 0 <= newRow < ROWS and 0 <= newCol < COLS and (board[newRow][newCol] == "." or board[newRow][newCol].islower() != piece.islower()):
+                moves.append((newRow, newCol))
 
     return moves
 
@@ -107,7 +111,7 @@ def main():
 
     selected_piece = None
     selected_position = None
-    legal_moves = []
+    legalMoves = []
 
     running = True
     while running:
@@ -123,22 +127,22 @@ def main():
                     if board[row][col] != ".":
                         selected_piece = board[row][col]
                         selected_position = (row, col)
-                        legal_moves = getLegalMoves(board, selected_position, selected_piece)
+                        legalMoves = getLegalMoves(board, selected_position, selected_piece)
 
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_pos = pygame.mouse.get_pos()
                 square = findCurrentSquare(board, mouse_pos)
                 if square and selected_piece:
-                    new_row, new_col = square
+                    newRow, newCol = square
 
-                    if (new_row, new_col) in legal_moves:
+                    if (newRow, newCol) in legalMoves:
                         old_row, old_col = selected_position
                         board[old_row][old_col] = "."
-                        board[new_row][new_col] = selected_piece
+                        board[newRow][newCol] = selected_piece
 
                     selected_piece = None
                     selected_position = None
-                    legal_moves = []
+                    legalMoves = []
 
         drawBoard(win)
         drawPieces(win, board, images)
@@ -146,7 +150,7 @@ def main():
         if selected_position:
             row, col = selected_position
             pygame.draw.rect(win, (255, 0, 0), (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 3)
-            highlightLegalMoves(win, legal_moves)
+            highlightLegalMoves(win, legalMoves)
 
         pygame.display.update()
         clock.tick(60)
